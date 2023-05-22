876cd971341df582cbf98b9e35932900255aced1
admin <syhai@atlantic.edu.vn>
2023-05-22 21:59:33 +0700

    commit

---
+++ lib/wiget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Table extends StatefulWidget {
+  const Table({Key? key}) : super(key: key);
+
+
+  @override
+  State<Table> createState() => _TableState();
+}
+
+class _TableState extends State<Table> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
